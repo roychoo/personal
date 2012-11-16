80fc3ec01d3b113bf4957cec114627db50862ee6
Roy Choo <roychoo@Roys-MacBook-Air.local>
2012-11-16 16:38:56 +0800

    Change port number

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(8000, function(){
+  console.log("Express server listening on port 8000";
 });
